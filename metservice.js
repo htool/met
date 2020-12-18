9e6dabc5b9042c2c89ef8aece4b5ceb41967324c
Hans Tool <hans@defect.org>
2020-12-18 08:38:34 +0100

    36 hours is old enough

--- metservice.js
+++ metservice.js
@@ -64,7 +64,7 @@ function removeOld () {
   console.log('Removing old');
   for (const [key, value] of Object.entries(charts)) {
     // console.log('Now: ' + now + ' key: ' + key);
-    if (now - key > (3600 * 1000 * 24 * 4)) { // 4 days
+    if (now - key > (3600 * 1000 * 36)) { // 4 days
       console.log('Deleting old ' + value['filename']);
       fs.unlink(chartsFolder + '/' + value['filename'], (err => {
         if (err) console.log(err);
